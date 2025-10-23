a4d8c2f9841a7554a4899bb132a83010c689e652
stevehall <steve.hall@atscale.com>
2025-10-23 10:33:07 -0700

    Improve AwsSecretsManager

--- src/main/com/atscale/java/utils/AwsSecretsManager.java
+++ src/main/com/atscale/java/utils/AwsSecretsManager.java
@@ -1,10 +1,6 @@
 package com.atscale.java.utils;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-
-import org.apache.commons.collections4.Get;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
@@ -17,13 +13,7 @@ public class AwsSecretsManager implements SecretsManager{
     }
 
     public String callAws(String... params) {
-        SecretsManagerClient client = null;
-
-        try {
-            client = SecretsManagerClient.builder()
-                    .region(Region.of(params[0]))
-                    .build();
-
+        try (SecretsManagerClient client = SecretsManagerClient.builder().region(Region.of(params[0])).build()) {
             GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
                     .secretId(params[1])
                     .build();
@@ -31,21 +21,10 @@ public class AwsSecretsManager implements SecretsManager{
             GetSecretValueResponse getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
 
             return getSecretValueResponse.secretString();
-        } finally {
-            if (client != null) {
-                client.close();
-            }
         }
     }
 
     protected Map<String, String> processAwsSecrets(String secretString) {
-        Map<String, String> flattenedSecrets = new HashMap<>();
-
-        Map<String, String> awsSecrets = JsonUtil.asMap(secretString);
-        for(String secretKey: awsSecrets.keySet()) {
-            flattenedSecrets.put(secretKey, awsSecrets.get(secretKey));
-
-        }
-        return flattenedSecrets;
+        return JsonUtil.asMap(secretString);
     }
 }
