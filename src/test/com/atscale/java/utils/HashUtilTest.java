e2f093dab3383005de17c68be06272a41b1c3c35
stevehall <steve.hall@atscale.com>
2025-11-17 07:14:29 -0800

    ATSCALE-42119 Have a working model that supports concurrent execution of Gatling tests against two separate systems via use of a second properties file.  Adding example use case in OpenStepConcurrentSimulationExecutor.  Adds alternate hash function and test case

---
+++ src/test/com/atscale/java/utils/HashUtilTest.java
@@ -0,0 +1,20 @@
+package com.atscale.java.utils;
+
+import org.junit.jupiter.api.*;
+
+public class HashUtilTest {
+
+    public @Test void testToMd5() {
+        String input = "test";
+        String expectedMd5 = "098f6bcd4621d373cade4e832627b4f6";
+        String actualMd5 = HashUtil.TO_MD5(input);
+        Assertions.assertEquals(expectedMd5, actualMd5);
+    }
+
+    public @Test void testToSha256() throws Exception {
+        String input = "test";
+        String expectedSha256 = "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08";
+        String actualSha256 = HashUtil.TO_SHA256(input);
+        Assertions.assertEquals(expectedSha256, actualSha256);
+    }
+}
